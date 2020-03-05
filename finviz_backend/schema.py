829e7cde670744b93b90d57d1696c923018a20d4
Krzysztof <krzysztof.gruszczynski@city.ac.uk>
2020-03-05 17:29:05 +0000

    new

--- finviz_backend/schema.py
+++ finviz_backend/schema.py
@@ -1,10 +1,13 @@
 # schema.py
 import datetime
 
+import pandas as pd
 from ariadne import QueryType, make_executable_schema, ObjectType, MutationType
 from ariadne.contrib.django.scalars import date_scalar, datetime_scalar
+from django.utils import timezone
 from graphql import GraphQLResolveInfo
 from iexfinance import stocks
+from pandas import np
 
 from portfolio.methods import buy_order
 from portfolio.models import *
@@ -34,6 +37,7 @@ type_defs = """
     
     type Portfolio {
         balance: [Float]!
+        history: [[Float]]!
         openPositions: [Position]!
     }
     
@@ -74,12 +78,35 @@ def resolve_buy(_, info: GraphQLResolveInfo, input):
 
 @portfolio.field("openPositions")
 def resolve_open_positions(obj: Portfolio, info: GraphQLResolveInfo):
-    return obj.position_set.all()
+    res = {}
+    for position in obj.position_set.filter(closed=None).order_by("opened"):
+        if res.get(position.stock.symbol):
+            res[position.stock.symbol].quantity += position.quantity
+        else:
+            res[position.stock.symbol] = position
+    return res.values()
 
 
 @portfolio.field("balance")
 def resolve_balance(obj: Portfolio, info: GraphQLResolveInfo):
     return obj.balance
+
+
+@portfolio.field("history")
+def resolve_history(obj: Portfolio, info: GraphQLResolveInfo):
+    positions = obj.position_set.all()
+    history = pd.DataFrame({"close": []})
+    for position in positions:
+        prices = stocks.get_historical_data(
+            position.stock.symbol, start=position.opened,
+            end=position.closed or timezone.now(), close_only=True, output_format="pandas"
+        )
+        values = prices.close
+        if not position.closed:
+            quote = stocks.Stock(position.stock.symbol).get_quote().get("latestPrice")
+            values = values.append(pd.Series({pd.Timestamp(timezone.now().date()): quote}))
+        history["close"] = history["close"].add(position.quantity * values, fill_value=0)
+    return list(zip([1000 * x.timestamp() for x in history.index], history.close.values))
 
 
 @stock.field("latestPrice")
