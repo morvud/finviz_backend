7ff0d0b85d3ec2bd858f4256caf25016b097c3aa
morvud <krzysztof.gruszczynski@city.ac.uk>
2020-03-29 18:47:07 +0100

    factors

--- finviz_backend/schema.py
+++ finviz_backend/schema.py
@@ -9,7 +9,8 @@ from django.utils import timezone
 from graphql import GraphQLResolveInfo
 from iexfinance import stocks, utils
 import numpy as np
-
+import pandas_datareader as web
+from scipy import stats
 from portfolio.methods import buy_order, sell_order
 from portfolio.models import *
 
@@ -46,6 +47,8 @@ type_defs = """
         openPositions: [Position]!
         sectors: [Sector]!
         sortino: Float!
+        beta: Float!
+        netExposure: Float!
     }
     
     type Sector {
@@ -130,16 +133,38 @@ def resolve_balance(obj: Portfolio, info: GraphQLResolveInfo):
 
 @portfolio.field("sortino")
 def resolve_sortino(obj: Portfolio, info: GraphQLResolveInfo):
-    positions = obj.position_set.order_by("opened")
-    df = stocks.get_historical_data(
-        list(positions.values_list("stock__symbol", flat=True)),
-        start=positions.first().opened,
-        end=timezone.now(),
-        output_format="pandas",
-        close_only=True,
-    )
+    history = obj.get_history()
+    returns = history.pct_change()
+    downside_returns = returns[returns < 0]
+    expected_returns = returns.mean()
+    down_stdev = downside_returns.std()
 
-    return 0
+    return float(expected_returns / down_stdev)
+
+
+@portfolio.field("beta")
+def resolve_beta(obj: Portfolio, info: GraphQLResolveInfo):
+    positions = obj.position_set.all()
+    tickers = [x.stock.symbol for x in positions]
+    wts = [(x.quantity * x.price) / obj.balance for x in positions]
+    price_data = web.get_data_yahoo(tickers, start="2013-01-01", end="2018-03-01")
+    price_data = price_data["Adj Close"]
+    ret_data = price_data.pct_change()[1:]
+    port_ret = (ret_data * wts).sum(axis=1)
+    benchmark_price = web.get_data_yahoo("SPY", start="2013-01-01", end="2018-03-01")
+    benchmark_ret = benchmark_price["Adj Close"].pct_change()[1:]
+    (beta, alpha) = stats.linregress(benchmark_ret.values, port_ret.values)[0:2]
+    return beta
+
+
+@portfolio.field("netExposure")
+def resolve_net_exposure(obj: Portfolio, info: GraphQLResolveInfo):
+    positions = obj.position_set.all()
+    wts = [
+        (x.quantity * x.price) / obj.balance * (1 if x.long else -1) for x in positions
+    ]
+    print(wts)
+    return sum(wts)
 
 
 @portfolio.field("sectors")
